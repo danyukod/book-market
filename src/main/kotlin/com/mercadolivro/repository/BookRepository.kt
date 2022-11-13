f1a41762afe782dbb39b23d53564a8aae468a0ff
Danilo Kodavara <danilo.kodavara@b2wdigital.com>
2022-11-13 19:23:14 -0300

    feat: adicionando paginacao nas chamadas get.

--- src/main/kotlin/com/mercadolivro/repository/BookRepository.kt
+++ src/main/kotlin/com/mercadolivro/repository/BookRepository.kt
@@ -3,12 +3,15 @@ package com.mercadolivro.repository
 import com.mercadolivro.enums.BookStatus
 import com.mercadolivro.model.BookModel
 import com.mercadolivro.model.CustomerModel
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 
 
 @Repository
-interface BookRepository : CrudRepository<BookModel, Int>{
-    abstract fun findByStatus(ativo: BookStatus): List<BookModel>
+interface BookRepository : JpaRepository<BookModel, Int>{
+    abstract fun findByStatus(ativo: BookStatus, pageable: Pageable): Page<BookModel>
     fun findByCustomer(customer: CustomerModel): List<BookModel>
 }
