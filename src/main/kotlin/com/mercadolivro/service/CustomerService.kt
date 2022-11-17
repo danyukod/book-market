d497edd08eacf7dfbe2ec570509e2d48f115bfd4
Danilo Kodavara <danilo.kodavara@b2wdigital.com>
2022-11-16 23:57:24 -0300

    ci: Criando ControllerAdvice, e criando NotFoundException para lancar nas classes de servico.

--- src/main/kotlin/com/mercadolivro/service/CustomerService.kt
+++ src/main/kotlin/com/mercadolivro/service/CustomerService.kt
@@ -1,6 +1,7 @@
 package com.mercadolivro.service
 
 import com.mercadolivro.enums.CustomerStatus
+import com.mercadolivro.exception.NotFoundException
 import com.mercadolivro.model.CustomerModel
 import com.mercadolivro.repository.CustomerRepository
 import org.springframework.data.domain.Page
@@ -25,7 +26,9 @@ class CustomerService(
     }
 
     fun findById(id: Int): CustomerModel {
-        return customerRepository.findById(id).orElseThrow()
+        return customerRepository.findById(id).orElseThrow {
+            NotFoundException("Customer ${id} not exists.", "ML-0001")
+        }
     }
 
     fun update(customer: CustomerModel) {
