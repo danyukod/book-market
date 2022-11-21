534b3c8a02a9ee7a3fcf3851473f64abe3089419
Danilo Kodavara <danilo.kodavara@b2wdigital.com>
2022-11-21 11:39:03 -0300

    feat: adicionando spring doc ao projeto.

---
+++ src/main/kotlin/com/mercadolivro/config/SpringdocConfig.kt
@@ -0,0 +1,24 @@
+package com.mercadolivro.config
+
+import io.swagger.v3.oas.models.ExternalDocumentation
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.info.License
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+
+@Configuration
+class SpringdocConfig {
+
+    @Bean
+    fun api(): OpenAPI = OpenAPI().info(
+        Info().title("Mercado Livro")
+            .description("Aplicacao Mercado Livro")
+            .version("v0.0.1")
+            .license(License().name("Mercado Livro").url("http://mercadolivro.com.br")))
+        .externalDocs(ExternalDocumentation().description("Mercado Livro Documentation").url(
+            "http://mercadolivro.wiki.github.org/docs"
+        ))
+
+}
