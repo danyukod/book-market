94f292a2ae49659fc5b043cc7c5f1620855d5714
Danilo Kodavara <danilo.kodavara@hotmail.com>
2023-01-11 22:30:14 -0300

    feat: criando testes para customerRepository

--- src/test/kotlin/com/mercadolivro/repository/CustomerRepositoryTest.kt
+++ src/test/kotlin/com/mercadolivro/repository/CustomerRepositoryTest.kt
@@ -2,8 +2,9 @@ package com.mercadolivro.repository
 
 import com.mercadolivro.helper.buildCustomer
 import io.mockk.junit5.MockKExtension
-import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
@@ -23,11 +24,57 @@ class CustomerRepositoryTest {
     fun `should return name containing` () {
         val danilo = customerRepository.save(buildCustomer(name = "Danilo"))
         val daniel = customerRepository.save(buildCustomer(name = "Daniel"))
-        val thomas = customerRepository.save(buildCustomer(name = "Thomas"))
+        customerRepository.save(buildCustomer(name = "Thomas"))
 
         val customers = customerRepository.findByNameContaining("Da")
 
         assertEquals(listOf(danilo, daniel), customers)
+    }
+
+    @Nested
+    inner class `exists by email` {
+        @Test
+        fun `should return true when email exists`() {
+            val email = "email@teste.com"
+            customerRepository.save(buildCustomer(email = email))
+
+            val exists = customerRepository.existsByEmail(email)
+
+            assertTrue(exists)
+        }
+
+        @Test
+        fun `should return false when email do not exists`() {
+            val email = "noexistemail@teste.com"
+
+            val exists = customerRepository.existsByEmail(email)
+
+            assertFalse(exists)
+        }
+
+    }
+
+    @Nested
+    inner class `find by email` {
+        @Test
+        fun `should return customer when email exists`() {
+            val email = "email@teste.com"
+            val customer = customerRepository.save(buildCustomer(email = email))
+
+            val result = customerRepository.findByEmail(email)
+
+            assertNotNull(result)
+            assertEquals(customer, result)
+        }
+
+        @Test
+        fun `should return null when email do not exists`() {
+            val email = "noexistemail@teste.com"
+
+            val result = customerRepository.findByEmail(email)
+
+            assertNull(result)
+        }
 
     }
 }
